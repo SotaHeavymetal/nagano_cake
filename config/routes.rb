2a4fe9209e1f70336c76164736e1bc6c720f3eca
EC2 Default User <ec2-user@ip-172-31-15-237.ec2.internal>
2022-02-14 14:51:06 +0000

    [update]ルーティング、各種コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :customers
+  root to: 'homes#top'
+  get 'home/about'=>'homes#about'
+  resources :items, only: [:index, :show]
+  resources :cart_items
+  resources :orders
+  namespace :admin do
+    resources :genres
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
