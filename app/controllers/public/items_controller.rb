34b53772bc8495302d94f7d9b7262cd0db7d2533
EC2 Default User <ec2-user@ip-172-31-15-237.ec2.internal>
2022-03-18 13:47:28 +0000

    [update]検索機能、ジャンル検索機能修正

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,13 +1,21 @@
 class Public::ItemsController < ApplicationController
 
   def index
+    @genres = Genre.all
     #@item = Item.all
-     @items = Item.page(params[:page]).per(8)
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @items = @genre.items.page(params[:page]).per(8)
+    else
+      @items = Item.page(params[:page]).per(8)
+    end
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
+    @genres = Genre.all
+    @genre = Genre.find(params[:id])
   end
 
   def search
@@ -18,6 +26,6 @@ class Public::ItemsController < ApplicationController
 
     private
      def item_params
-      params.require(:cart_item).permit(:customer_id, :item_id, :amount)
+      params.require(:cart_item).permit(:customer_id, :item_id, :amount, :genre_id)
      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
     root 'homes#top'
     get '/about'=>'homes#about'
     get '/thanks'=>'homes#thanks'
+    resources :genres, only: [:show]
     resources :items, only: [:index, :show]
     get 'customers/my_page' => 'customers#show', as: :mypage
     get 'customers/unsubscribe'=>'customers#unsubscribe'
